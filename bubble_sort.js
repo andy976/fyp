69d9e42c16d8bbece3f53ea694563ca9f80541b8
andy976 <44550063+andy976@users.noreply.github.com>
2021-04-14 12:03:21 +0800

    Add files via upload

--- bubble_sort.js
+++ bubble_sort.js
@@ -16,7 +16,7 @@ function bubble_manual()
 {
     c_delay=0;
     for (var i = 0; i < array_size; i++){
-        origin_array[i] = JSON.parse(JSON.stringify(div_sizes[i]));
+        origin_array[i] = JSON.parse(JSON.stringify(number_size[i]));
     }
 
     action_count = 0;
@@ -25,43 +25,43 @@ function bubble_manual()
     {
         for(var j=0;j<array_size-i-1;j++)
         {
-            div_update_manual(JSON.parse(JSON.stringify(divs[j])),JSON.parse(JSON.stringify(div_sizes[j])),"yellow",j);//Color update
+            div_update_manual(JSON.parse(JSON.stringify(numberbar[j])),JSON.parse(JSON.stringify(number_size[j])),"yellow",j);//Color update
 
-            if(div_sizes[j]>div_sizes[j+1])
+            if(number_size[j]>number_size[j+1])
             {
                 
-                div_update_manual(JSON.parse(JSON.stringify(divs[j])),JSON.parse(JSON.stringify(div_sizes[j])), "red",j);//Color update
-                div_update_manual(JSON.parse(JSON.stringify(divs[j+1])),JSON.parse(JSON.stringify(div_sizes[j+1])), "red",j+1);//Color update
+                div_update_manual(JSON.parse(JSON.stringify(numberbar[j])),JSON.parse(JSON.stringify(number_size[j])), "red",j);//Color update
+                div_update_manual(JSON.parse(JSON.stringify(numberbar[j+1])),JSON.parse(JSON.stringify(number_size[j+1])), "red",j+1);//Color update
 
-                var temp=div_sizes[j];
-                div_sizes[j]=div_sizes[j+1];
-                div_sizes[j+1]=temp;
+                var temp=number_size[j];
+                number_size[j]=number_size[j+1];
+                number_size[j+1]=temp;
 
-                //var temp2=div_sizes[j].textContent;
-                //div_sizes[j].textContent=div_sizes[j+1].textContent;
-                //div_sizes[j+1].textContent=temp2.textContent;
-                msg_update_bubble_manual(JSON.parse(JSON.stringify(div_sizes[j+1])), JSON.parse(JSON.stringify(div_sizes[j])), action_count);
-                div_update_manual(JSON.parse(JSON.stringify(divs[j])),JSON.parse(JSON.stringify(div_sizes[j])), "red",j);//Height update
-                div_update_manual(JSON.parse(JSON.stringify(divs[j+1])),JSON.parse(JSON.stringify(div_sizes[j+1])), "red",j+1);//Height update
+                //var temp2=number_size[j].textContent;
+                //number_size[j].textContent=number_size[j+1].textContent;
+                //number_size[j+1].textContent=temp2.textContent;
+                msg_update_bubble_manual(JSON.parse(JSON.stringify(number_size[j+1])), JSON.parse(JSON.stringify(number_size[j])), action_count);
+                div_update_manual(JSON.parse(JSON.stringify(numberbar[j])),JSON.parse(JSON.stringify(number_size[j])), "red",j);//Height update
+                div_update_manual(JSON.parse(JSON.stringify(numberbar[j+1])),JSON.parse(JSON.stringify(number_size[j+1])), "red",j+1);//Height update
                 
-                //document.getElementById("Info_Cont1").innerHTML = div_sizes[j+1] + " swapped with " + div_sizes[j];
+                //document.getElementById("Info_Cont1").innerHTML = number_size[j+1] + " swapped with " + number_size[j];
             }
-            div_update_manual(JSON.parse(JSON.stringify(divs[j])),JSON.parse(JSON.stringify(div_sizes[j])), "blue",j);//Color update
+            div_update_manual(JSON.parse(JSON.stringify(numberbar[j])),JSON.parse(JSON.stringify(number_size[j])), "blue",j);//Color update
         }
-        div_update_manual(JSON.parse(JSON.stringify(divs[j])),JSON.parse(JSON.stringify(div_sizes[j])), "green",j);//Color update
+        div_update_manual(JSON.parse(JSON.stringify(numberbar[j])),JSON.parse(JSON.stringify(number_size[j])), "green",j);//Color update
     }
-    div_update_manual(JSON.parse(JSON.stringify(divs[0])),JSON.parse(JSON.stringify(div_sizes[0])), "green",0);//Color update
+    div_update_manual(JSON.parse(JSON.stringify(numberbar[0])),JSON.parse(JSON.stringify(number_size[0])), "green",0);//Color update
 
     /*cont.innerHTML="";
 
     for(var i=0;i<array_size;i++)
     {
-        div_sizes[i] = origin_array[i];
-        divs[i]=document.createElement("div");
-        cont.appendChild(divs[i]);
+        number_size[i] = origin_array[i];
+        numberbar[i]=document.createElement("div");
+        cont.appendChild(numberbar[i]);
         margin_size=0.1;
-        divs[i].innerHTML = "<div align=\"center\"><font color = white>" + div_sizes[i] + "</font></div>";
-        divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+        numberbar[i].innerHTML = "<div align=\"center\"><font color = white>" + number_size[i] + "</font></div>";
+        numberbar[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (number_size[i]) + "%;";
     }*/
 
     //enable_buttons();
@@ -76,31 +76,31 @@ function bubble_auto()
     {
         for(var j=0;j<array_size-i-1;j++)
         {
-            div_update(divs[j],div_sizes[j],"yellow");//Color update
+            div_update(numberbar[j],number_size[j],"yellow");//Color update
 
-            if(div_sizes[j]>div_sizes[j+1])
+            if(number_size[j]>number_size[j+1])
             {
-                div_update(divs[j],div_sizes[j], "red");//Color update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Color update
+                div_update(numberbar[j],number_size[j], "red");//Color update
+                div_update(numberbar[j+1],number_size[j+1], "red");//Color update
 
-                var temp=div_sizes[j];
-                div_sizes[j]=div_sizes[j+1];
-                div_sizes[j+1]=temp;
+                var temp=number_size[j];
+                number_size[j]=number_size[j+1];
+                number_size[j+1]=temp;
 
-                //var temp2=div_sizes[j].textContent;
-                //div_sizes[j].textContent=div_sizes[j+1].textContent;
-                //div_sizes[j+1].textContent=temp2.textContent;
-                msg_update_bubble(div_sizes[j+1], div_sizes[j]);
-                div_update(divs[j],div_sizes[j], "red");//Height update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Height update
+                //var temp2=number_size[j].textContent;
+                //number_size[j].textContent=number_size[j+1].textContent;
+                //number_size[j+1].textContent=temp2.textContent;
+                msg_update_bubble(number_size[j+1], number_size[j]);
+                div_update(numberbar[j],number_size[j], "red");//Height update
+                div_update(numberbar[j+1],number_size[j+1], "red");//Height update
                 
-                //document.getElementById("Info_Cont1").innerHTML = div_sizes[j+1] + " swapped with " + div_sizes[j];
+                //document.getElementById("Info_Cont1").innerHTML = number_size[j+1] + " swapped with " + number_size[j];
             }
-            div_update(divs[j],div_sizes[j], "blue");//Color update
+            div_update(numberbar[j],number_size[j], "blue");//Color update
         }
-        div_update(divs[j],div_sizes[j], "green");//Color update
+        div_update(numberbar[j],number_size[j], "green");//Color update
     }
-    div_update(divs[0],div_sizes[0], "green");//Color update
+    div_update(numberbar[0],number_size[0], "green");//Color update
 
     enable_buttons();
 }
